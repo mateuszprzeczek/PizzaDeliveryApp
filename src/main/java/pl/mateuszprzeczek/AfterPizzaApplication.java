2e4c8148d5d05c1c3e3470c539469ebbcab02ed3
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2020-06-19 16:32:28 +0200

    a new project after-pizza has been generated

---
+++ src/main/java/pl/mateuszprzeczek/AfterPizzaApplication.java
@@ -0,0 +1,15 @@
+package pl.mateuszprzeczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterPizzaApplication.class, args);
+		
+	
+	}
+
+}
